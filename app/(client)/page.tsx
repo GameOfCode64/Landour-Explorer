a29c0567855aa95403d761ef2e1f7dc9fed1f5d2
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2025-02-03 22:08:08 +0530

    project setup

---
+++ app/(client)/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Home() {
+  return <main className="w-full h-full"></main>;
+}
---
+++ app/(client)/treks/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TrekPage = () => {
+  return <div>TrekPage</div>;
+};
+
+export default TrekPage;
