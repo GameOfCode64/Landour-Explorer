7d7474255eca3cf00b19edc43ecde70a13b8f9c4
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2025-02-08 16:33:47 +0530

    add search

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { Menu, Mountain, Search } from "lucide-react";
@@ -9,8 +10,10 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { useSearch } from "@/hooks/use-search";
 
 const Navbar = () => {
+  const { onOpen } = useSearch();
   return (
     <div className="w-full md:h-[90px] h-[70px]">
       <div className="flex justify-between items-center h-full px-4 md:px-12">
@@ -18,7 +21,6 @@ const Navbar = () => {
           <Mountain className="text-ocean-green-500" />
           <h1 className="text-lg mt-1 font-bold ">Landour Explorer</h1>
         </Link>
-
         <div className="md:flex hidden items-center justify-normal gap-8">
           <Link href="/" className="font-semibold text-[17px]">
             Home
@@ -35,15 +37,19 @@ const Navbar = () => {
           <Link href="/" className="font-semibold text-[17px]">
             Contact
           </Link>
-          <Search className="cursor-pointer" />
+          <Search className="cursor-pointer" onClick={onOpen} />
         </div>
         <div className="md:hidden flex items-center justify-normal gap-4">
-          <Search className="cursor-pointer" />
+          <Search className="cursor-pointer" onClick={onOpen} />
           <Sheet>
             <SheetTrigger>
               <Menu className="cursor-pointer" />
             </SheetTrigger>
             <SheetContent>
+              <SheetHeader>
+                <SheetTitle></SheetTitle>
+                <SheetDescription></SheetDescription>
+              </SheetHeader>
               <div className="flex flex-col items-center mt-12 justify-normal gap-8">
                 <Link href="/" className="font-semibold text-[17px]">
                   Home
