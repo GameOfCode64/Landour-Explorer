4dea39e9989c254e83109ae0e71513b35bb0a2d6
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2025-02-10 21:25:49 +0530

    ui changes

--- app/(client)/blogs/page.tsx
+++ app/(client)/blogs/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const BlogPage = () => {
-  return <div className="md:px-20 px-4">BlogPage</div>;
+  return (
+    <div className="md:px-20 px-4">
+      <h1 className="text-2xl md:text-4xl font-bold text-ocean-green-500">
+        Our Latest Blogs
+      </h1>
+    </div>
+  );
 };
 
 export default BlogPage;
